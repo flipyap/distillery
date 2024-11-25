6c9c80a63960e5265267228d979705ecd0a4a096
Erik Kristensen <erik@erikkristensen.com>
2024-11-25 08:16:27 -0700

    chore(scripts): fix installer for powershell

--- scripts/installer.tpl.ps1
+++ scripts/installer.tpl.ps1
@@ -28,22 +28,22 @@ $trap = {
 $OS = "windows" # Hardcoded for Windows
 $ARCH = if ([System.Environment]::Is64BitProcess) { "amd64" } else { "arm64" }
 
-$TAR_FILE = "${FILE_BASENAME}-${VERSION}-${OS}-${ARCH}.tar.gz"
+$ZIP_FILE = "${FILE_BASENAME}-${VERSION}-${OS}-${ARCH}.zip"
 
 # Download distillery
 Write-Host "Downloading distillery $VERSION..."
-Invoke-WebRequest -Uri "$RELEASES_URL/download/$VERSION/$TAR_FILE" -OutFile "$TMP_DIR\$TAR_FILE"
+Invoke-WebRequest -Uri "$RELEASES_URL/download/$VERSION/$ZIP_FILE" -OutFile "$TMP_DIR\$ZIP_FILE"
 Invoke-WebRequest -Uri "$RELEASES_URL/download/$VERSION/checksums.txt" -OutFile "$TMP_DIR\checksums.txt"
 
 # Verify checksums
 Write-Host "Verifying checksums..."
-$checksums = Get-Content "$TMP_DIR\checksums.txt" | Where-Object { $_ -match $TAR_FILE }
+$checksums = Get-Content "$TMP_DIR\checksums.txt" | Where-Object { $_ -match $ZIP_FILE }
 if (-not $checksums) {
-    Write-Error "Checksum not found for $TAR_FILE"
+    Write-Error "Checksum not found for $ZIP_FILE"
     exit 1
 }
 $expected = $checksums.Split(" ")[0]
-$actual = (Get-FileHash -Path "$TMP_DIR\$TAR_FILE" -Algorithm SHA256).Hash
+$actual = (Get-FileHash -Path "$TMP_DIR\$ZIP_FILE" -Algorithm SHA256).Hash
 if ($expected -ne $actual) {
     Write-Error "Checksum verification failed!"
     exit 1
@@ -65,7 +65,7 @@ if (Get-Command cosign -ErrorAction SilentlyContinue) {
 
 # Extract tar file
 Write-Host "Extracting distillery..."
-Expand-Archive -Path "$TMP_DIR\$TAR_FILE" -DestinationPath $TMP_DIR -Force
+Expand-Archive -Path "$TMP_DIR\$ZIP_FILE" -DestinationPath $TMP_DIR -Force
 
 # Run the installation command
 Write-Host "Installing distillery..."
